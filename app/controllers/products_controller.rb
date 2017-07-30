27df7ec33fc737ff451cfff0a5a946edfcb40cdb
Fernando Flores <fernando.flores1961@gmail.com>
2017-07-29 22:47:44 -0500

    Add Ajax for Cart rendering and channel to update product info

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,8 +42,12 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to @product, notice: 'Se actualizó exitosamente el producto.' }
         format.json { render :show, status: :ok, location: @product }
+
+        @products = Product.all
+        ActionCable.server.broadcast 'products',
+          html: render_to_string('store/index', layout: false)
       else
         format.html { render :edit }
         format.json { render json: @product.errors, status: :unprocessable_entity }
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -48,4 +48,15 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to line_items_url
   end
+
+  test "should create line_item via Ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:complan).id }, xhr: true
+    end
+
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Complan/
+    end
+  end
 end
