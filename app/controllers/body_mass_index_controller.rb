40a1e4dd52692f5edc24e3ebe9596683fe1dcb72
Fernando Flores <fernando.flores1961@gmail.com>
2018-01-05 22:40:54 -0600

    Fix Tabla Nutricia presentation and buttons aestethics

--- app/controllers/body_mass_index_controller.rb
+++ app/controllers/body_mass_index_controller.rb
@@ -8,22 +8,17 @@ class BodyMassIndexController < ApplicationController
      "Bajo peso"                                    => { desde: 16.1, hasta: 18.5 },
      "Normal (peso saludable)"                      => { desde: 18.51, hasta: 25.0 },
      "Sobre peso"                                   => { desde: 25.1, hasta: 30.0 },
-     "Obesidad clase I (moderadamente obeso)"       => { desde: 30.1, hasta: 35.0 },
-     "Obesidad clase II (severamente obeso)"        => { desde: 35.1, hasta: 40.0 },
-     "Obesidad class III (Obesidad muy severa)"     => { desde: 40.1, hasta: 60.0 },
+     "Obesidad moderada (clase I)"                  => { desde: 30.1, hasta: 35.0 },
+     "Obesidad severa (clase II)"                   => { desde: 35.1, hasta: 40.0 },
+     "Obesidad muy severa (clase III)"              => { desde: 40.1, hasta: 60.0 },
      "Factor de grasa pura"                         => { desde: 60.1, hasta: 100.0}
    }
    weight = params[:mass].to_f
     height = params[:height].to_f
     if weight > 0 && height > 0
-      puts weight
-      puts height
       resultado = ''
       @bmi = weight / height ** 2
       bmi_table.each do |advice, range|
-        puts advice
-        puts range[:desde], range[:hasta]
-        puts @bmi
         if (@bmi > range[:desde]) && (@bmi < range[:hasta])
           resultado = advice
         end
